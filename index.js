2aefe62e21854959dd2408fc5d72c39db1e7a864
Josh Johnston <josh@x-team.com>
2014-10-17 12:43:10 +1100

    fixed comment style for compatibility with cdocparser

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/*
+/**
 
 Overview
 ====
@@ -43,7 +43,7 @@ var through = require('through');
 var processFile = require('./lib/process-file');
 var renderDocInfo = require('./lib/render-doc-info');
 
-/*
+/**
 
 Default options
 ----
