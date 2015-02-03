80946a697180a66bd991af85dcd651860c3de058
Bernardo Dias <bernardodiasdacruz@gmail.com>
2015-02-03 21:32:09 -0200

    javacript rewritten

--- template/default.js
+++ template/default.js
@@ -1,65 +1,118 @@
 /*
+  Template Script
+  =====
 
-  Table of Contents
-  -----
-
+  This script builds all functionality on the top of the content created by inline-docs.
 */
+(function( global ) {
+  "use strict";
 
-function toArray(obj) {
-  var array = new Array(obj.length);
-  for (var i = 0; i < obj.length; i++)
-    array[i] = obj[i];
-  return array;
-}
+  var InlineDocs = (function() {
 
-function createAnchorElem (headingElem) {
-  var elem = document.createElement('a');
-  elem.href = '#' + headingElem.id;
-  elem.innerHTML = headingElem.innerHTML;
+    var contentColumn,
+        navigationColumn;
 
-  return elem;
-}
+    return {
+      init: function() {
+        buildLayout();
+        iterateHeaders();
+      }
+    };
 
-function addAnchor (headingElem) {
-  var id = headingElem.id;
-  if (!id) { return; }
+    /*
+      Building the layout
+      -----
 
-  var anchorElem = createAnchorElem(headingElem);
-  headingElem.innerHTML = '';
-  headingElem.appendChild(anchorElem);
-}
+      Markup of the layout:
 
-var ToC = document.createElement('div');
-var sections = document.getElementsByTagName('section');
-sections = toArray(sections);
+      ```
+      <layout>
+        <row>
+          <column class="navigation"></column>
+          <column class="content"></column>
+        <row>
+      <layout>
 
-tocHtml = "<nav role='navigation' class='table-of-contents'><strong>Table of Contents</strong><ul>";
+      ```
+    */
+    function buildLayout() {
+      var layout = document.createElement("layout"),
+          content = document.body.firstElementChild;
 
-tocHtml += sections.map(function(section) {
-  var headerOne = section.getElementsByTagName('h1');
-  headerOne = toArray(headerOne);
+      // > Wrap the content created by inline-docs with the template markup
+      layout.innerHTML = "<row>"
+                       + "<column class='navigation'></column>"
+                       + "<column class='content'></column>"
+                       + "</row>";
+      document.body.firstElementChild.remove();
+      document.body.insertBefore(layout, document.body.firstChild);
+      contentColumn = document.querySelectorAll("column.content")[0];
+      contentColumn.appendChild(content);
+    }
 
-  var sectionHtml = "<li><a href='#" + headerOne[0].id + "'>" + headerOne[0].innerHTML + "</a>";
-  addAnchor(headerOne[0]);
+    /*
+      Iterate headers
+      -----
 
-  var headersTwo = section.getElementsByTagName('h2');
-  headersTwo = toArray(headersTwo);
+      Look for `h1`, `h2`, `h3`, `h4`, `h5` and `h6` elements inside content.
+    */
+    function iterateHeaders() {
+      var parentLevel = 0,
+          navigationMarkup = "";
 
-  if (headersTwo.length > 0) {
-    sectionHtml += "<ul>";
-    sectionHtml += headersTwo.map(function(h2) {
-      addAnchor(h2);
-      return "<li><a href='#" + h2.id + "'>" + h2.innerHTML + "</a>";;
-    }).join('\n');
-    sectionHtml += "</ul>";
-  }
+      [].forEach.call(contentColumn.querySelectorAll("h1, h2, h3, h4, h5, h6"), function(header) {
+        var currentLevel,
+            headerText;
 
-  sectionHtml += "</li>";
-  return sectionHtml;
-}).join('\n');
+        // > Trigger addAnchor(header)
+        addAnchor(header);
 
-ToC.id = 'table-of-contents';
-ToC.innerHTML = tocHtml;
+        // > Check header level hierarchy to build the markup
+        currentLevel = header.tagName.substring(1);
 
-var firstHeading = document.querySelectorAll('h1')[0];
-firstHeading.parentNode.insertBefore(ToC, firstHeading.nextSibling);
+        if (currentLevel > parentLevel) {
+          navigationMarkup += "<ul class='level-" + currentLevel + "'>";
+        } else if (currentLevel === parentLevel) {
+          navigationMarkup += "</li>";
+        } else {
+          navigationMarkup += "</li></ul></li>";
+        }
+
+        // > Include current level link
+        headerText = header.innerText || header.textContent;
+        navigationMarkup += "<li class='level-" + currentLevel + "'><a href='#" + header.id + "'>" + headerText  + "</a>";
+        parentLevel = currentLevel;
+      });
+
+      navigationMarkup += '</li></ul>';
+
+      // > Include the navigation markup
+      navigationColumn = document.querySelectorAll("column.navigation")[0];
+      navigationColumn.innerHTML = navigationMarkup;
+    }
+
+    /*
+      Add anchor to all header with proper id
+      -----
+    */
+    function addAnchor(header) {
+      var anchor = document.createElement("a");
+
+      // > If the header don't have an Id, create one
+      if (header.id === "" || header.id === "undefined") {
+          header.id = header.innerHTML.toLowerCase().replace(/[^\w]+/g, "-");
+      }
+
+      // > Wrap the header in the anchor
+      anchor.href = "#" + header.id;
+      anchor.innerHTML = header.innerHTML;
+      header.innerHTML = "";
+      header.appendChild(anchor);
+    }
+
+  })();
+
+  global.InlineDocs = InlineDocs;
+  InlineDocs.init();
+
+})( this );
